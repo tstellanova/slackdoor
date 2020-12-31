9f331fc3b856e776d06da46bdfc7fd87d2fb35b3
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-12-31 09:09:08 -0800

    better support for usb power

--- src/slackdoor.cpp
+++ src/slackdoor.cpp
@@ -4,68 +4,95 @@
  * Author: Todd Stellanova
  */
 
-#include <Particle.h>
+#include "Particle.h"
+#include "DiagnosticsHelperRK.h"
+#include "system_power.h"
 
 // We select this system mode in order to control _when_ the potentially
 // lengthy network connection process runs.
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-const uint16_t g_user_led = D7; 
-const uint16_t g_button_input = D2;
+const uint16_t USER_LED_PIN = D7; 
+const uint16_t BUTTON_PIN = D2;
+const String EMPTY_EVT_DATA = String(2);
+static PinState g_cur_led_state = LOW;
+volatile bool g_button_pressed = false;
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Configure button input pin as pullup-- when switch is clicked it will fall to ground
-  pinMode(g_user_led, OUTPUT);
-  pinMode(g_button_input, INPUT_PULLUP);
-}
-
-// runs forever after setup
-void loop() {
-  PinState cur_led_state = LOW;
-  digitalWrite(g_user_led, cur_led_state);
-
-  // sleep until the doorbell is pressed
-  SystemSleepConfiguration sleep_cfg;
-  sleep_cfg.mode(SystemSleepMode::ULTRA_LOW_POWER)
-        .gpio(g_button_input, FALLING);
-  SystemSleepResult result = System.sleep(sleep_cfg);
-
-  // TODO check whether we awoke specifically due to button click
-  // (sleep may end for other reasons)
-  cur_led_state = HIGH;
-  digitalWrite(g_user_led, HIGH);
+void publish_doorbell_event() {
+  g_cur_led_state = HIGH;
+  digitalWrite(USER_LED_PIN, HIGH);
   // provide a little time to catch a DFU over USB
   delay(100);
-
 
   // Regardless of the network technology, it takes some time to connect to the Particle cloud:
   // blink the LED while connecting
   if (!Particle.connected()) {
     Particle.connect();
     while (!Particle.connected()) {
-      cur_led_state = (HIGH == cur_led_state) ? LOW : HIGH;
-      digitalWrite(g_user_led, cur_led_state);
+      g_cur_led_state = (HIGH == g_cur_led_state) ? LOW : HIGH;
+      digitalWrite(USER_LED_PIN, g_cur_led_state);
       Particle.process();
       delay(100);
     }
   }
 
-  // Placeholder data (unused)
-  String data = String(2);
   // Trigger the webhook integration
-  Particle.publish("evt_door", data, PRIVATE);
+  Particle.publish("evt_door", EMPTY_EVT_DATA, PRIVATE);
   delay(50);
 
-  // blink the LED to indicate the bell has been rung
+  // blink the LED quickly to indicate the bell has been rung
   for (int i = 0; i < 30; i++) {
     Particle.process();
     delay(500);
-    cur_led_state = (HIGH == cur_led_state) ? LOW : HIGH;
-    digitalWrite(g_user_led, cur_led_state);
+    g_cur_led_state = (HIGH == g_cur_led_state) ? LOW : HIGH;
+    digitalWrite(USER_LED_PIN, g_cur_led_state);
   }
 
   // gracefully shutdown the cloud connection
   Particle.disconnect();
+}
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Configure button input pin as pullup-- when switch is clicked it will fall to ground
+  pinMode(USER_LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+// Runs forever after setup()
+void loop() {
+  g_cur_led_state = LOW;
+  digitalWrite(USER_LED_PIN, g_cur_led_state);
+  
+  int32_t powerSource = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_POWER_SOURCE);
+  if (POWER_SOURCE_BATTERY == powerSource) {
+    // sleep until the doorbell is pressed
+    SystemSleepConfiguration sleep_cfg;
+    sleep_cfg.mode(SystemSleepMode::ULTRA_LOW_POWER)
+          .gpio(BUTTON_PIN, FALLING);
+    SystemSleepResult result = System.sleep(sleep_cfg);
+
+    // check whether we awoke specifically due to button click
+    if (SystemSleepWakeupReason::BY_GPIO == result.wakeupReason()) {
+      publish_doorbell_event();
+    }
+  }
+  else {
+    if (g_button_pressed) {
+      publish_doorbell_event();
+    }
+    else {
+      g_cur_led_state = HIGH;
+      digitalWrite(USER_LED_PIN, g_cur_led_state);
+      delay(1000);
+      g_cur_led_state = LOW;
+      digitalWrite(USER_LED_PIN, g_cur_led_state);
+      delay(1000);
+    }
+  }
+
+
+ 
+
+
 }
       
