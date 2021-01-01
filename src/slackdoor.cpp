364f9527ed11af5d8fe09ba4bd427092250d6fa9
Todd Stellanova <tstellanova@users.noreply.github.com>
2021-01-01 11:46:10 -0800

    better power mode detection

--- src/slackdoor.cpp
+++ src/slackdoor.cpp
@@ -5,8 +5,6 @@
  */
 
 #include "Particle.h"
-#include "DiagnosticsHelperRK.h"
-#include "system_power.h"
 
 // We select this system mode in order to control _when_ the potentially
 // lengthy network connection process runs.
@@ -14,10 +12,13 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 
 const uint16_t USER_LED_PIN = D7; 
 const uint16_t BUTTON_PIN = D2;
-const String EMPTY_EVT_DATA = String(2);
+const String EMPTY_EVT_DATA = String(0);
 static PinState g_user_led_state = LOW;
 volatile bool g_button_pressed = false;
 
+SerialLogHandler g_log_handler(LOG_LEVEL_WARN, { // Logging level for non-application messages
+    { "app", LOG_LEVEL_INFO } // Logging level for application messages
+});
 
 void set_user_led_state(PinState mode) {
     g_user_led_state = mode;
@@ -49,27 +50,47 @@ void indicate_doorbell_idle() {
 // Regardless of the network technology, it takes some time to connect to the Particle cloud:
 // blink the LED while connecting
 void wait_for_connection() {
-  while (!Particle.connected()) {
-    togger_user_led_state();
-    Particle.process();
-    delay(100);
+  if (!Particle.connected()) {
+    Particle.connect();
+    while (!Particle.connected()) {
+      togger_user_led_state();
+      Particle.process();
+      delay(100);
+    }
   }
+
 }
 
 // Someone had pressed the doorbell: publish an event to the Particle Cloud
 void publish_doorbell_event() {
   set_user_led_state(HIGH);
 
-  if (!Particle.connected()) {
-    Particle.connect();
-    wait_for_connection();
-  }
+  wait_for_connection();
 
   // Trigger the webhook integration
-  Particle.publish("evt_door", EMPTY_EVT_DATA, PRIVATE);
+  Particle.publish("household/frontdoor/bell02", EMPTY_EVT_DATA, PRIVATE);
   delay(50);
 
   indicate_doorbell_working();
+}
+
+// detect whether we're running on battery power
+bool battery_powered() {
+  #if(PLATFORM_ID == PLATFORM_ARGON)
+    // check whether we have external (non-battery) power
+    int usb_pwr_val = digitalRead(PWR);
+    Log.info("usb_pwr_val: %d", usb_pwr_val);
+    return (LOW == usb_pwr_val);
+  #elif(PLATFORM_ID == PLATFORM_BORON)
+    // check whether we are discharging the battery 
+    int batt_state = System.batteryState();
+    Log.info("batt_state: %d", batt_state);
+    float batt_soc = System.batteryCharge();
+    Log.info("soc: %.1f", batt_soc);
+    return (BATTERY_STATE_DISCHARGING == batt_state);
+  #else
+  #error "Unsupported platform"
+  #endif
 }
 
 // Interrupt service routine called when button is pressed:
@@ -83,22 +104,25 @@ void setup() {
   pinMode(USER_LED_PIN, OUTPUT);
   // Configure button input pin as pullup-- when button is pressed it will fall to ground
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  #if(PLATFORM_ID == PLATFORM_ARGON)
+  pinMode(PWR, INPUT);
+  #endif
+
+  Log.info(" -- BEGIN --");
 }
 
 // Runs forever after setup()
 void loop() {
-  g_user_led_state = LOW;
-  digitalWrite(USER_LED_PIN, g_user_led_state);
+  set_user_led_state(LOW);
   
-  int32_t powerSource = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_POWER_SOURCE);
-  if (POWER_SOURCE_BATTERY == powerSource) {
+  if (battery_powered())  {
     // cleanup interrupt in case it's previosly attached
     detachInterrupt(BUTTON_PIN);
 
     // sleep until the doorbell is pressed
     SystemSleepConfiguration sleep_cfg;
     sleep_cfg.mode(SystemSleepMode::ULTRA_LOW_POWER)
-          .network(9)
           .gpio(BUTTON_PIN, FALLING);
     SystemSleepResult result = System.sleep(sleep_cfg);
 
@@ -108,6 +132,7 @@ void loop() {
     }
   }
   else {
+
     if (g_button_pressed) {
       g_button_pressed = false;
       publish_doorbell_event();
@@ -115,6 +140,8 @@ void loop() {
     else {
       // detect when button is pressed
       attachInterrupt(BUTTON_PIN, isr_button_pressed, FALLING);
+      // ensure that we're connected to the network, awaiting updates
+      wait_for_connection();
       indicate_doorbell_idle();
     }
   }
