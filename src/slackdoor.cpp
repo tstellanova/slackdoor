4197d0a3499eb117698e66feaaea29f1302bdd8d
Todd Stellanova <tstellanova@users.noreply.github.com>
2021-01-02 15:15:38 -0800

    Support for detecting battery or USB power (#1)
    * better support for usb power
    
    * cleanup LED state indication
    
    * better power mode detection
    
    * minor tweaks

--- src/slackdoor.cpp
+++ src/slackdoor.cpp
@@ -4,68 +4,147 @@
  * Author: Todd Stellanova
  */
 
-#include <Particle.h>
+#include "Particle.h"
 
 // We select this system mode in order to control _when_ the potentially
 // lengthy network connection process runs.
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-const uint16_t g_user_led = D7; 
-const uint16_t g_button_input = D2;
+const uint16_t USER_LED_PIN = D7; 
+const uint16_t BUTTON_PIN = D2;
+const String EMPTY_EVT_DATA = String(0);
+static PinState g_user_led_state = LOW;
+volatile bool g_button_pressed = false;
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Configure button input pin as pullup-- when switch is clicked it will fall to ground
-  pinMode(g_user_led, OUTPUT);
-  pinMode(g_button_input, INPUT_PULLUP);
+SerialLogHandler g_log_handler(LOG_LEVEL_WARN, { // Logging level for non-application messages
+    { "app", LOG_LEVEL_INFO } // Logging level for application messages
+});
+
+void set_user_led_state(PinState mode) {
+    g_user_led_state = mode;
+    digitalWrite(USER_LED_PIN, g_user_led_state);
 }
 
-// runs forever after setup
-void loop() {
-  PinState cur_led_state = LOW;
-  digitalWrite(g_user_led, cur_led_state);
+void togger_user_led_state() {
+    PinState next_state = (HIGH == g_user_led_state) ? LOW : HIGH;
+    set_user_led_state(next_state);
+}
 
-  // sleep until the doorbell is pressed
-  SystemSleepConfiguration sleep_cfg;
-  sleep_cfg.mode(SystemSleepMode::ULTRA_LOW_POWER)
-        .gpio(g_button_input, FALLING);
-  SystemSleepResult result = System.sleep(sleep_cfg);
+// Blink the LED quickly to indicate the bell has been rung
+void indicate_doorbell_working() {
+  for (int i = 0; i < 30; i++) {
+    Particle.process();
+    delay(500);
+    togger_user_led_state();
+  }
+}
 
-  // TODO check whether we awoke specifically due to button click
-  // (sleep may end for other reasons)
-  cur_led_state = HIGH;
-  digitalWrite(g_user_led, HIGH);
-  // provide a little time to catch a DFU over USB
-  delay(100);
+// Blink the LED slowly to indicate we're waiting for a doorbell press
+void indicate_doorbell_idle() {
+  for (int i = 0; i < 2; i++) {
+    togger_user_led_state();
+    delay(1000);
+  }
+}
 
-
-  // Regardless of the network technology, it takes some time to connect to the Particle cloud:
-  // blink the LED while connecting
+// Regardless of the network technology, it takes some time to connect to the Particle cloud:
+// blink the LED while connecting
+void wait_for_connection() {
   if (!Particle.connected()) {
     Particle.connect();
     while (!Particle.connected()) {
-      cur_led_state = (HIGH == cur_led_state) ? LOW : HIGH;
-      digitalWrite(g_user_led, cur_led_state);
+      togger_user_led_state();
       Particle.process();
       delay(100);
     }
   }
 
-  // Placeholder data (unused)
-  String data = String(2);
+}
+
+// Someone had pressed the doorbell: publish an event to the Particle Cloud
+void publish_doorbell_event() {
+  set_user_led_state(HIGH);
+
+  wait_for_connection();
+
   // Trigger the webhook integration
-  Particle.publish("evt_door", data, PRIVATE);
+  Particle.publish("household/frontdoor/bell01", EMPTY_EVT_DATA, PRIVATE);
   delay(50);
 
-  // blink the LED to indicate the bell has been rung
-  for (int i = 0; i < 30; i++) {
-    Particle.process();
-    delay(500);
-    cur_led_state = (HIGH == cur_led_state) ? LOW : HIGH;
-    digitalWrite(g_user_led, cur_led_state);
+  indicate_doorbell_working();
+}
+
+// detect whether we're running on battery power
+bool battery_powered() {
+  #if(PLATFORM_ID == PLATFORM_ARGON)
+    // check whether we have external (non-battery) power
+    int usb_pwr_val = digitalRead(PWR);
+    Log.info("usb_pwr_val: %d", usb_pwr_val);
+    return (LOW == usb_pwr_val);
+  #elif(PLATFORM_ID == PLATFORM_BORON)
+    // check whether we are discharging the battery 
+    int batt_state = System.batteryState();
+    Log.info("batt_state: %d", batt_state);
+    float batt_soc = System.batteryCharge();
+    Log.info("soc: %.1f", batt_soc);
+    return (BATTERY_STATE_DISCHARGING == batt_state);
+  #else
+  #error "Unsupported platform"
+  #endif
+}
+
+// Interrupt service routine called when doorbell button is pressed:
+// We don't do much processing here, just note that the button was pressed
+void isr_button_pressed() {
+  g_button_pressed = true;
+}
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  pinMode(USER_LED_PIN, OUTPUT);
+  // Configure button input pin as pullup-- when button is pressed it will fall to ground
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  #if(PLATFORM_ID == PLATFORM_ARGON)
+  pinMode(PWR, INPUT);
+  #endif
+
+  Log.info(" -- BEGIN --");
+}
+
+// Runs forever after setup()
+void loop() {
+  set_user_led_state(LOW);
+  
+  if (battery_powered())  {
+    // cleanup interrupt in case it's previously attached
+    detachInterrupt(BUTTON_PIN);
+
+    // sleep until the doorbell is pressed
+    SystemSleepConfiguration sleep_cfg;
+    sleep_cfg.mode(SystemSleepMode::ULTRA_LOW_POWER)
+          .gpio(BUTTON_PIN, FALLING);
+    SystemSleepResult result = System.sleep(sleep_cfg);
+
+    // check whether we awoke specifically due to button click
+    if (SystemSleepWakeupReason::BY_GPIO == result.wakeupReason()) {
+      publish_doorbell_event();
+    }
+  }
+  else {
+    if (g_button_pressed) {
+      g_button_pressed = false;
+      publish_doorbell_event();
+    }
+    else {
+      // ensure that we're connected to the network
+      wait_for_connection();
+      // detect when button is pressed
+      attachInterrupt(BUTTON_PIN, isr_button_pressed, FALLING);
+      indicate_doorbell_idle();
+    }
   }
 
-  // gracefully shutdown the cloud connection
-  Particle.disconnect();
+
 }
       
