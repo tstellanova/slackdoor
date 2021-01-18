71c0eb5aae0b856e127d2eca991906dcb25c3ceb
Todd Stellanova <tstellanova@users.noreply.github.com>
2021-01-18 14:24:54 -0800

    minor

--- src/slackdoor.cpp
+++ src/slackdoor.cpp
@@ -11,53 +11,17 @@
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
 const pin_t USER_LED_PIN = D7; 
-const pin_t BUTTON_PIN = D2;
+const pin_t DOORBELL_BUTTON_PIN = D2;
 const String EMPTY_EVT_DATA = String(0);
-
-// PWM audio output pin
-const uint16_t SPKR_PIN = D3;
-// approximate tone frequencies:
-const unsigned int TONE_A4 = 440;
-const unsigned int TONE_B4 = 494;
-const unsigned int TONE_C4 = 262;
-const unsigned int TONE_D4 = 294;
-const unsigned int TONE_E4 = 330;
-const unsigned int TONE_F4 = 349;
-const unsigned int TONE_G4 = 392;
-
-const int NOTE_DURATION_MS = 500;
+const system_tick_t BUTTON_DEBOUNCE_TIME = 3000;
 
 static PinState g_user_led_state = LOW;
 static volatile bool g_button_pressed = false;
+static system_tick_t last_trigger_time = 0;
 
 SerialLogHandler g_log_handler(LOG_LEVEL_WARN, { // Logging level for non-application messages
     { "app", LOG_LEVEL_INFO } // Logging level for application messages
 });
-
-
-// play a tone and wait for it to complete before moving on
-void sync_play_tone(unsigned int frequency, int milliseconds) {
-    if (frequency != 0) {
-        tone(SPKR_PIN, frequency, milliseconds);
-    }
-    else {
-        noTone(SPKR_PIN);
-    }
-    delay(milliseconds);
-}
-
-void play_bell_tone() {
-    for (int count= 0; count < 1; count++) {
-        // tinny "close encounters"
-        sync_play_tone(2*TONE_D4, NOTE_DURATION_MS);
-        sync_play_tone(2*TONE_E4, NOTE_DURATION_MS);
-        sync_play_tone(2*TONE_C4, NOTE_DURATION_MS);
-        sync_play_tone(TONE_C4, NOTE_DURATION_MS);
-        sync_play_tone(TONE_G4, 2*NOTE_DURATION_MS);
-        sync_play_tone(0, 2*NOTE_DURATION_MS);
-    }
-    noTone(SPKR_PIN);
-}
 
 // force the user LED to a particular state
 void set_user_led_state(PinState mode) {
@@ -82,10 +46,8 @@ void indicate_doorbell_working() {
 
 // Blink the LED slowly to indicate we're waiting for a doorbell press
 void indicate_doorbell_idle() {
-  for (int i = 0; i < 2; i++) {
     togger_user_led_state();
     delay(1000);
-  }
 }
 
 // Regardless of the network technology, it takes some time to connect to the Particle cloud:
@@ -104,14 +66,13 @@ void wait_for_connection() {
 
 // Someone had pressed the doorbell: publish an event to the Particle Cloud
 void publish_doorbell_event() {
-  play_bell_tone();
   set_user_led_state(HIGH);
   wait_for_connection();
 
   // Trigger the webhook integration
   Particle.publish("household/frontdoor/bell01", EMPTY_EVT_DATA, PRIVATE);
-  delay(50);
-
+  // send vital statistics to the cloud every time we get a doorbell push
+  Particle.publishVitals();
   indicate_doorbell_working();
 }
 
@@ -136,16 +97,22 @@ bool battery_powered() {
 // Interrupt service routine called when doorbell button is pressed:
 // We don't do much processing here, just note that the button was pressed
 void isr_button_pressed() {
-  g_button_pressed = true;
+  //verify that the button press duration is at least some bare minimum
+  int32_t live_button = pinReadFast(DOORBELL_BUTTON_PIN);
+  if (live_button == HIGH) {
+    system_tick_t cur_time = millis();
+    if ((cur_time - last_trigger_time) > BUTTON_DEBOUNCE_TIME) {
+      last_trigger_time = cur_time;
+      g_button_pressed = true;
+    }
+  }
 }
 
 // setup() runs once, when the device is first turned on.
 void setup() {
   pinMode(USER_LED_PIN, OUTPUT);
-  pinMode(SPKR_PIN, OUTPUT);// PWM tone output pin
-
   // Configure button input pin as pulldown-- when button is pressed it will connect to +V and rise
-  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
+  pinMode(DOORBELL_BUTTON_PIN, INPUT_PULLDOWN);
 
   #if(PLATFORM_ID == PLATFORM_ARGON)
   pinMode(PWR, INPUT);
@@ -160,17 +127,19 @@ void loop() {
   
   if (battery_powered())  {
     // cleanup interrupt in case it's previously attached
-    detachInterrupt(BUTTON_PIN);
+    detachInterrupt(DOORBELL_BUTTON_PIN);
 
     // sleep until the doorbell is pressed
     SystemSleepConfiguration sleep_cfg;
     sleep_cfg.mode(SystemSleepMode::ULTRA_LOW_POWER)
-          .gpio(BUTTON_PIN, RISING);
+        .gpio(DOORBELL_BUTTON_PIN, RISING) //wakeup when doorbell is pushed
+        .duration(120s); //... or after some time
+
     SystemSleepResult result = System.sleep(sleep_cfg);
 
     // check whether we awoke specifically due to button click
     if ((SystemSleepWakeupReason::BY_GPIO == result.wakeupReason()) &&
-        (BUTTON_PIN == result.wakeupPin())) {
+        (DOORBELL_BUTTON_PIN == result.wakeupPin())) {
       publish_doorbell_event();
     }
   }
@@ -183,7 +152,7 @@ void loop() {
       // ensure that we're connected to the network
       wait_for_connection();
       // detect when button is pressed
-      attachInterrupt(BUTTON_PIN, isr_button_pressed, RISING);
+      attachInterrupt(DOORBELL_BUTTON_PIN, isr_button_pressed, RISING);
       indicate_doorbell_idle();
     }
   }
